626fab4d3ce3ce3041ef4d0c076b2abe3480c9be
afrometal <radek.kwlsk@gmail.com>
2017-12-02 12:55:18 +0100

    iterate in planner, handle results over channel, multiple ants over multiple goroutines

--- gotravel/gotravelsvc/planner.go
+++ gotravel/gotravelsvc/planner.go
@@ -6,13 +6,14 @@ import (
 	"sync"
 	"time"
 
+	"github.com/kr/pretty"
 	"googlemaps.github.io/maps"
 )
 
 const (
-	Iterations = 1
+	Iterations = 20
 	Boost      = 5
-	Ants       = 1
+	Ants       = 10
 )
 
 type Planner struct {
@@ -29,7 +30,8 @@ func (planner *Planner) Evaluate() (indexes []int, err error) {
 	var times *TravelTimeMatrix
 	var distances *DistanceMatrix
 	var pheromones *PheromonesMatrix
-	var pherMutex = &sync.Mutex{}
+	var pherMutex = sync.Mutex{}
+	var resultChannel = make(chan Result)
 
 	{
 		random := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -40,27 +42,40 @@ func (planner *Planner) Evaluate() (indexes []int, err error) {
 
 		for r := 1; r < length; r++ {
 			for c := 1; c < length && r != c; c++ {
-				times.Set(r, c, time.Duration(random.Intn(20)+10)*time.Minute)
+				times.Set(r, c, time.Now(), time.Duration(random.Intn(10)+10)*time.Minute)
 				distances.Set(r, c, float64(random.Intn(10)+5))
 			}
 		}
 
-		pheromones = NewPheromonesMatrix(length, float64(Boost))
+		pheromones = NewPheromonesMatrix(length, float64(Boost), pherMutex)
 	}
 
 	var bestPath = NewDummyPath()
 	var bestTime = time.Duration(math.MaxInt64)
 
-	for i := 1; i <= Ants; i++ {
-		ant := NewAnt(planner.trip, distances, times, pheromones, pherMutex)
-		err = ant.FindFood(Iterations, Boost)
-		if err != nil {
-			return indexes, err
+	var ants [Ants]*Ant
+
+	for i := 0; i < Ants; i++ {
+		ants[i] = NewAnt(planner.trip, distances, times, pheromones, resultChannel)
+	}
+	for i := 0; i < Iterations; i++ {
+		for i := 0; i < Ants; i++ {
+			go ants[i].FindFood(Boost)
 		}
-		if ant.totalTime <= bestTime {
-			bestPath = *ant.BestPath()
-			bestTime = ant.totalTime
+		for i := 0; i < Ants; i++ {
+			result := <-resultChannel
+			if result.time <= bestTime {
+				pretty.Printf(
+					"better result! time: %.2f minutes, priorities: %d\n",
+					float64(result.time/time.Minute),
+					result.priorities,
+				)
+				pretty.Println(result.path.PathIndexes())
+				bestPath = result.path
+				bestTime = result.time
+			}
 		}
+		pheromones.Evaporate(Boost, Iterations)
 	}
 
 	return bestPath.PathIndexes(), err
